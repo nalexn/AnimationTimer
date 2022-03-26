6cc11c2154bbdff6c3bd9620dd4b903d7209b505
Alexey Naumov <alexey@naumov.tech>
2022-03-26 11:11:53 +0300

    Initial commit

---
+++ AnimationTimer/SceneDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  SceneDelegate.swift
+//  AnimationTimer
+//
+//  Created by Alexey on 26.03.2022.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
---
+++ AnimationTimer/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  AnimationTimer
+//
+//  Created by Alexey on 26.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
