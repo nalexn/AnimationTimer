e4552f4870b0d356896e6ca9d4f58b92de49f5b9
Alexey Naumov <alexey@naumov.tech>
2022-03-26 16:32:41 +0300

    Change animation

--- AnimationTimer/EssentialCode/SampleAnimation.swift
+++ AnimationTimer/EssentialCode/SampleAnimation.swift
@@ -71,32 +71,43 @@ final class SampleAnimation<T>: UIView where T: AnimationTimer {
 
 private extension Timer.Tick {
     func shapePath(size: CGSize, startTime: CFTimeInterval) -> UIBezierPath {
-        let path = UIBezierPath()
-        let sec: CGFloat = 4 // full revolution in seconds
+        let sec: CGFloat = 2 // full revolution in seconds
         let twoPi: CGFloat = 2 * .pi
-        let angle1 = twoPi * CGFloat(timestamp - startTime) / sec
-        let angle2 = angle1 + twoPi / 3
-        let angle3 = angle2 + twoPi / 3
-        let angle4 = angle1 + twoPi
-        let revRatio = angle1 / twoPi
+        let angle = twoPi * CGFloat(timestamp - startTime) / sec
+        
+        let revRatio = angle / twoPi
         let progress = revRatio - floor(revRatio)
-        let factor = 1 - abs(2 * progress - 1)
-        let angle12 = angle1 * (1 - factor) + angle2 * factor
-        let angle23 = angle2 * (1 - factor) + angle3 * factor
-        let angle34 = angle3 * (1 - factor) + angle4 * factor
-        path.move(to: size.point(angle: angle1))
-        path.addQuadCurve(to: size.point(angle: angle2), controlPoint: size.point(angle: angle12))
-        path.addQuadCurve(to: size.point(angle: angle3), controlPoint: size.point(angle: angle23))
-        path.addQuadCurve(to: size.point(angle: angle4), controlPoint: size.point(angle: angle34))
-        path.close()
+
+        let circleRadius = size.width * 0.1
+        let mainRadius = min(size.width, size.height) * 0.5 - circleRadius
+        let path = UIBezierPath()
+        
+        distribution(progress: progress, count: 8)
+            .map { $0 * twoPi - 0.5 * .pi }
+            .map { CGPoint(x: size.width * 0.5 + mainRadius * cos($0),
+                           y: size.height * 0.5 + mainRadius * sin($0)) }
+            .map { UIBezierPath(arcCenter: $0, radius: circleRadius, startAngle: 0, endAngle: twoPi, clockwise: false) }
+            .forEach { path.append($0) }
         return path
+    }
+    
+    private func distribution(progress: CGFloat, count: Int) -> [CGFloat] {
+        let _max = progress * 2
+        let step = 1 / CGFloat(count)
+        var positions: [CGFloat] = []
+        var current = _max
+        for _ in 0 ..< count {
+            positions.append(max(0, min(1, current)))
+            current -= step
+        }
+        return positions
     }
 }
 
 private extension CGSize {
     func point(angle: CGFloat) -> CGPoint {
         let x: CGFloat = (1 + cos(angle)) / 2
-        let y: CGFloat = (1 - sin(angle)) / 2
+        let y: CGFloat = (1 + sin(angle)) / 2
         return CGPoint(x: x * width, y: y * height)
     }
 }
