0fdd6be0819bb0648d83baab13ac4b410e011f25
Alexey Naumov <alexey@naumov.tech>
2022-03-26 13:42:05 +0300

    Add timers and animation view

---
+++ AnimationTimer/EssentialCode/Timers.swift
@@ -0,0 +1,132 @@
+//
+//  TimerProtocol.swift
+//  Timers
+//
+//  Created by Alexey on 26.03.2022.
+//
+
+import QuartzCore
+import UIKit.UIScreen
+
+protocol AnimationTimer {
+    
+    func start()
+    func stop()
+    
+    init(onTimer: @escaping (Timer.Tick) -> Void)
+}
+
+struct Timer { } // Just a namespace
+
+extension Timer {
+    struct Tick {
+        let timestamp: TimeInterval
+        let frameDiration: TimeInterval
+        
+        init(last: Tick?) {
+            let timestamp = CACurrentMediaTime()
+            self.timestamp = timestamp
+            self.frameDiration = last.flatMap({ timestamp - $0.timestamp }) ?? 0
+        }
+    }
+}
+
+// MARK: - DisplayLink
+
+extension Timer {
+    final class DisplayLink: AnimationTimer {
+        
+        private let timer: CADisplayLink
+        private weak var target: Target?
+        
+        init(onTimer: @escaping (Tick) -> Void) {
+            let target = Target(onTimer: onTimer)
+            self.target = target
+            timer = CADisplayLink(target: target, selector: #selector(Target.tick(_:)))
+            timer.isPaused = true
+            timer.add(to: .current, forMode: .common)
+        }
+        
+        deinit {
+            timer.invalidate()
+        }
+        
+        func start() {
+            target?.last = nil
+            timer.isPaused = false
+        }
+        
+        func stop() {
+            timer.isPaused = true
+        }
+        
+        var isRunning: Bool {
+            return !timer.isPaused
+        }
+        
+        fileprivate final class Target {
+            
+            let onTimer: (Tick) -> Void
+            var last: Tick?
+            
+            init(onTimer: @escaping (Tick) -> Void) {
+                self.onTimer = onTimer
+            }
+            
+            @objc fileprivate func tick(_ link: CADisplayLink) {
+                let tick = Tick(last: last)
+                self.last = tick
+                onTimer(tick)
+            }
+        }
+    }
+}
+
+// MARK: - DispatchSourceTimer
+
+extension Timer {
+    final class DispatchSourceTimer: AnimationTimer {
+        
+        private static var queue = DispatchQueue(
+            label: "animation.timer", qos: .userInteractive, autoreleaseFrequency: .workItem)
+        private let timer: Dispatch.DispatchSourceTimer
+        private(set) var isRunning: Bool = false
+        private var last: Tick?
+        
+        init(onTimer: @escaping (Tick) -> Void) {
+            timer = DispatchSource.makeTimerSource(flags: [.strict], queue: Self.queue)
+            let maxFPS = UIScreen.main.maximumFramesPerSecond
+            let timeInterval = 1 / TimeInterval(maxFPS)
+            timer.schedule(deadline: .now(), repeating: timeInterval)
+            timer.setEventHandler(handler: { [weak self] in
+                let tick = Tick(last: self?.last)
+                self?.last = tick
+                autoreleasepool {
+                    CATransaction.begin()
+                    onTimer(tick)
+                    CATransaction.commit()
+                }
+            })
+        }
+        
+        deinit {
+            timer.setEventHandler {}
+            timer.cancel()
+            // https://forums.developer.apple.com/thread/15902
+            start()
+        }
+        
+        func start() {
+            guard !isRunning else { return }
+            isRunning = true
+            last = Tick(last: nil)
+            timer.resume()
+        }
+        
+        func stop() {
+            guard isRunning else { return }
+            isRunning = false
+            timer.suspend()
+        }
+    }
+}
--- AnimationTimer/ViewController.swift
+++ AnimationTimer/ViewController.swift
@@ -7,9 +7,47 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    override func loadView() {
+        let stackView = UIStackView(frame: .zero)
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.alignment = .center
+        stackView.distribution = .equalSpacing
+        stackView.isLayoutMarginsRelativeArrangement = true
+        stackView.layoutMargins = UIEdgeInsets(top: 30, left: 10, bottom: 30, right: 10)
+        self.view = stackView
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        guard let stackView = self.view as? UIStackView else { return }
+        stackView.addArrangedSubview(AnimationCell(Timer.DisplayLink.self, name: "CADisplayLink"))
+        stackView.addArrangedSubview(AnimationCell(Timer.DispatchSourceTimer.self, name: "DispatchSourceTimer"))
+    }
+}
+
+final class AnimationCell: UIStackView {
+    
+    init<T>(_ timerType: T.Type, name: String) where T: AnimationTimer {
+        super.init(frame: .zero)
+        translatesAutoresizingMaskIntoConstraints = false
+        axis = .vertical
+        alignment = .center
+        distribution = .fill
+        let animation = SampleAnimation<T>()
+        let title = UILabel()
+        title.translatesAutoresizingMaskIntoConstraints = false
+        title.font = UIFont.monospacedSystemFont(ofSize: 30, weight: .light)
+        title.textColor = .secondaryLabel
+        addArrangedSubview(animation)
+        addArrangedSubview(title)
+        layoutIfNeeded()
+        animation.startAnimating()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
 }
