6fd635508f96f0ca7f71703ba42ff14fbed2397d
Alexey Naumov <alexey@naumov.tech>
2022-03-27 11:43:43 +0300

    Refactor AnimationView

--- AnimationTimer/EssentialCode/AnimationView.swift
+++ AnimationTimer/EssentialCode/AnimationView.swift
@@ -7,6 +7,15 @@
 
 import UIKit
 
+private extension Timer.Tick.AnimationParams {
+    struct Constants {
+        static var fullRevolutionInSeconds: CGFloat { 2 }
+        static var circleRadiusProportion: CGFloat { 0.07 }
+        static var numberOfCircles: Int { 12 }
+        static var gradient: [UIColor] { [.red, .blue] }
+    }
+}
+
 final class AnimationView<T>: UIView where T: AnimationTimer {
     
     private var timer: T!
@@ -16,12 +25,12 @@ final class AnimationView<T>: UIView where T: AnimationTimer {
         super.init(frame: .zero)
         translatesAutoresizingMaskIntoConstraints = false
         guard let gradient = self.layer as? CAGradientLayer else { fatalError() }
-        gradient.colors = [UIColor.red.cgColor, UIColor.blue.cgColor]
+        gradient.colors = Timer.Tick.AnimationParams.Constants.gradient.map { $0.cgColor }
         gradient.mask = shapeLayer
         let size = self.intrinsicContentSize
         timer = T(mainThread: mainThread) { [weak self] tick in
             guard let self = self else { return }
-            let params = tick.animationParams(size: size)
+            let params = Timer.Tick.AnimationParams(tick: tick, size: size)
             self.shapeLayer.path = params.shapePath.cgPath
             gradient.startPoint = params.gradientStart
             gradient.endPoint = params.gradientEnd
@@ -71,39 +80,44 @@ final class AnimationView<T>: UIView where T: AnimationTimer {
     deinit { stopAnimating() }
 }
 
+// MARK: - AnimationParams calculation
+
 private extension Timer.Tick {
-    
     struct AnimationParams {
         let shapePath: UIBezierPath
         let gradientStart: CGPoint
         let gradientEnd: CGPoint
     }
+}
+
+private extension Timer.Tick.AnimationParams {
     
-    func animationParams(size: CGSize) -> AnimationParams {
-        let sec: CGFloat = 2 // full revolution in seconds
+    init(tick: Timer.Tick, size: CGSize) {
+        typealias Constants = Timer.Tick.AnimationParams.Constants
+        
         let twoPi: CGFloat = 2 * .pi
-        let angle = twoPi * CGFloat(timestamp - startTime) / sec
+        let angle = twoPi * CGFloat(tick.timestamp - tick.startTime) / Constants.fullRevolutionInSeconds
         
         let revRatio = angle / twoPi
-        let progress = revRatio - floor(revRatio)
+        let revProgress = revRatio - floor(revRatio)
 
-        let circleRadius = size.width * 0.1
-        let mainRadius = min(size.width, size.height) * 0.5 - circleRadius
+        let circleRadius = size.width * Constants.circleRadiusProportion
+        let revRadius = min(size.width, size.height) * 0.5 - circleRadius
         let path = UIBezierPath()
         
-        distribution(progress: progress, count: 8)
+        Self.distribution(progress: revProgress, count: Constants.numberOfCircles)
             .map { $0 * twoPi - 0.5 * .pi }
-            .map { CGPoint(x: size.width * 0.5 + mainRadius * cos($0),
-                           y: size.height * 0.5 + mainRadius * sin($0)) }
+            .map { CGPoint(x: size.width * 0.5 + revRadius * cos($0),
+                           y: size.height * 0.5 + revRadius * sin($0)) }
             .map { UIBezierPath(arcCenter: $0, radius: circleRadius, startAngle: 0, endAngle: twoPi, clockwise: false) }
             .forEach { path.append($0) }
         let gradientAngle = angle - .pi * 0.5
         let gradientStart = CGPoint(x: sin(gradientAngle), y: cos(gradientAngle))
         let gradientEnd = CGPoint(x: sin(gradientAngle + .pi), y: cos(gradientAngle + .pi))
-        return AnimationParams(shapePath: path, gradientStart: gradientStart, gradientEnd: gradientEnd)
+        self.init(shapePath: path, gradientStart: gradientStart, gradientEnd: gradientEnd)
     }
     
-    private func distribution(progress: CGFloat, count: Int) -> [CGFloat] {
+    private static func distribution(progress: CGFloat, count: Int) -> [CGFloat] {
         let _max = progress * 2
         let step = 1 / CGFloat(count - 1)
         var positions: [CGFloat] = []
